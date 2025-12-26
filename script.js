5242db6f478e2d9d5f014dff87fd8b317a172064
memoryofgenerationssy-eng <memory.of.generations.sy@gmail.com>
2025-12-27 02:47:38 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const savedTheme = localStorage.getItem('theme') || 'dark';
+document.body.classList.add(savedTheme);
 /* ===== THEME TOGGLE ===== */
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
